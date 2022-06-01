666530d227dba11e29fe9749d7ad153dd6287453
tschjens <104820772+tschjens@users.noreply.github.com>
2022-06-01 18:06:34 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,18 +2,51 @@
 #include <stdlib.h>
 
 
-struct lines{
-    int numbers[9];
-};
+#include <stdio.h>
+#include <stdlib.h>
 
-struct sudoku{
-    struct lines line[9];
-};
+
+void displaysudoku(int sudokuzahlen[9][9]);
+
 int main()
 {
+    int sudokuzahlen[9][9] = {0};
+    displaysudoku(sudokuzahlen);
+/*    char auswahl = ' ';
+    do{
+        printf("+++Sudoku+++\n");
+        printf("n = Neues Spiel\n");
+        printf("x = Spiel beenden\n");
+        scanf("%c",&auswahl);
+    }while(auswahl != 'x' && auswahl != 'n');
 
-    displaySudoku();
+    if(auswahl == 'n' || auswahl == 'N')
+    {
+        printf("1 = leichtes  Spiel\n");
+        printf("1 = mittleres Spiel\n");
+        printf("1 = schweres  Spiel\n");
+        printf("1 = Eigenes Sudoku importieren\n");
+
+    }
+
+
+*/
     return 0;
 }
 
+void displaysudoku(int sudokuzahlen[9][9])
+{
+    for(int i = 0; i < 14; i++)
+    {
+        for(int j = 0; j < 26; j++)
+        {
+            if(i == 0)
+            {
+                if(j % 8 == 0)
+                printf("|");
+            }
+        }
+    }
 
+
+}
